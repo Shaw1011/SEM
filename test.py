21a300d5314ee63004980a36a18b91c987689192
Edge Shaw <149878403+Shaw1011@users.noreply.github.com>
2024-07-30 00:12:59 +0530

    test.py

--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-def has_duplicates(input_list):
-    return len(input_list) != len(set(input_list))
-my_list = input("Enter the values: ").split()
-print(has_duplicates(my_list))
+# all the codes first checked here, then only they went for their specific files.
